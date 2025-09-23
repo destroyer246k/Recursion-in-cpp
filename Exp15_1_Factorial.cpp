ea7c36014daae2efbdfccbbc1566038a63458f4f
Patel Vishwas <149381035+destroyer246k@users.noreply.github.com>
2025-09-23 06:24:16 -0400

    Create Exp15_1_Factorial.cpp

---
+++ Exp15_1_Factorial.cpp
@@ -0,0 +1,20 @@
+//Patel Vishwas
+//24070123072
+//A3
+#include <iostream>
+using namespace std;
+int factorial(int n){
+    if(n==0){
+        return 1;
+    }
+    else{
+        return n*factorial(n-1);
+    }
+}
+int main() {
+    int n;
+    cout << "Enter Number to find factorial: ";
+    cin >> n;
+    cout << "Factorial is " << factorial(n);
+    return 0;
+}
